8e38ee0accf2f533b2a712d6599f9e3d9ea5d026
Ketley-Souza <ketleysouza55@gmail.com>
2025-05-12 23:47:26 -0300

    Dashboard ADM feita

--- app/dashboard/ADM/page.tsx
+++ app/dashboard/ADM/page.tsx
@@ -5,53 +5,13 @@ import { useRouter } from "next/navigation";
 import { authClient } from "@/lib/auth-client";
 import { toast } from "react-hot-toast";
 
-
 import Image from 'next/image';
 import Link from 'next/link';
-import UserProfile from '../components/userProfile';
+//import UserProfile from '../components/userProfile';
 
-
-export default function DashboardPage() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function checkAuth() {
-      try {
-        
-        // Verifica se o usuário está autenticado
-        const session = await authClient.getSession();
-
-        console.log("Resposta da sessão:", session);
-
-        if (!session || !session || !session.data?.user) {
-          alert("Usuário não autenticado, redirecionando para a página de login.");
-          router.push("/sign-in");
-        } else {
-          toast.success(`Sessão verificada com sucesso, seja bem-vindo(a) ao seu Dashboard, ${session.data.user.name}!`);
-          setLoading(false); // Liberar o conteúdo
-        }
-      } catch (error) {
-        console.error("Erro ao verificar sessão:", error);
-        router.push("/");
-      }
-    }
-
-    checkAuth();
-  }, [router]);
-
-  async function handleSignOut() {
-    await authClient.signOut();
-    router.push("/sign-in");
-  }
-
-  if (loading) {
-    return <div>Carregando...</div>;
-  }
-
+export default function AdminDashboardPage() {
   return (
-    <>    
-      <div className="flex flex-row justify-center w-full bg-white [background:linear-gradient(90deg,rgba(7,21,49,1)_0%,rgba(7,22,50,1)_6%,rgba(7,24,53,1)_13%,rgba(6,27,58,1)_19%,rgba(6,32,64,1)_25%,rgba(5,37,72,1)_31%,rgba(5,44,80,1)_38%,rgba(4,50,90,1)_44%,rgba(4,58,100,1)_50%,rgba(3,65,110,1)_56%,rgba(2,78,128,1)_69%,rgba(1,83,136,1)_75%,rgba(1,88,142,1)_81%,rgba(0,91,147,1)_88%)]  ">
+   <div className="flex flex-row justify-center w-full bg-white [background:linear-gradient(90deg,rgba(7,21,49,1)_0%,rgba(7,22,50,1)_6%,rgba(7,24,53,1)_13%,rgba(6,27,58,1)_19%,rgba(6,32,64,1)_25%,rgba(5,37,72,1)_31%,rgba(5,44,80,1)_38%,rgba(4,50,90,1)_44%,rgba(4,58,100,1)_50%,rgba(3,65,110,1)_56%,rgba(2,78,128,1)_69%,rgba(1,83,136,1)_75%,rgba(1,88,142,1)_81%,rgba(0,91,147,1)_88%)]  ">
         <div className="w-full max-w-[1200px] min-h-screen flex flex-col items-start p-4 lg:p-8 mx-auto relative">
           <div className="w-full max-w-[700px] lg:max-w-[1330px] flex flex-col items-start mt-14">
             <div className="flex justify-between items-center w-full mb-8">
@@ -73,7 +33,7 @@ export default function DashboardPage() {
                 />  A imagem esta com mais qualidade que a do user abaixo, mas se eu mexer vai da problema
                 */}
                 
-                <UserProfile style='flex-row' />
+                {/*<UserProfile style='flex-row' />*/}
                 
                 <Image
                   src="/Login/seta.png"
@@ -96,12 +56,14 @@ export default function DashboardPage() {
               <hr className="w-[120px] bg-white h-[3px] border-0 p-0 m-0 mb-4"/>
             </div>
 
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-[65%]">
-              {[    
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 w-full">
+              {[   
+                { img: "cad-adm.png", text: "Cadastros Gerais", path: "/agendamento" }, 
                 { img: "for.png", text: "Fórum", path: "/forum" },
+                { img: "age.png", text: "Agendamento", path: "/agendamento" }, /*   */
+                { img: "comu-adm.png", text: "Comunicação Interna", path: "/agendamento" },
                 { img: "fin.png", text: "Financeiro", path: "/financeiro" },
-                { img: "age.png", text: "Agendamento", path: "/agendamento" }, /*   */               
-                { img: "doc.png", text: "Documentos", path: "/documentos" },
+                { img: "imp-adm.png", text: "Impressão de relatórios", path: "/documentos" },
               ].map((item, index) => (
                 <Link
                   key={index}
@@ -111,7 +73,7 @@ export default function DashboardPage() {
                     width={58}
                     height={58}
                     alt={item.text}
-                    src={`/Login/morador/${item.img}`}
+                    src={`/Login/adm/${item.img}`}
                   />
                   <div className="[font-family:'Poppins',Helvetica] font-bold text-white text-base tracking-[0] leading-[normal]">
                     {item.text}
@@ -122,6 +84,5 @@ export default function DashboardPage() {
           </div>
         </div>
       </div>
-    </>
   );
 }
--- app/dashboard/financeiro/page.tsx
+++ app/dashboard/financeiro/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 
 import NavbarDashboard from "../components/navbarDashboard";
 
